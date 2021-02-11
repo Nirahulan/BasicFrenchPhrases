03358c50ecdce38dad29df9cbf79df59b4158a1e
Nirahulan <60203633+Nirahulan@users.noreply.github.com>
2021-02-11 02:30:26 -0500

    clean-up

--- app/src/main/java/com/myappcompany/nira/basicphrases/MainActivity.java
+++ app/src/main/java/com/myappcompany/nira/basicphrases/MainActivity.java
@@ -10,51 +10,45 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Main logic for buttons 
     public void phrases(View view){
+
 
         switch (view.getId()) {
 
             case R.id.button1:
-                // do your code
                 MediaPlayer hello = MediaPlayer.create(this, R.raw.hello);
                 hello.start();
                 break;
 
             case R.id.button2:
-                // do your code
                 MediaPlayer how_are_you = MediaPlayer.create(this, R.raw.how_are_u);
                 how_are_you.start();
                 break;
             case R.id.button3:
-                // do your code
                 MediaPlayer im_tired = MediaPlayer.create(this, R.raw.im_tired);
                 im_tired.start();
                 break;
 
             case R.id.button4:
-                // do your code
                 MediaPlayer very_good = MediaPlayer.create(this, R.raw.very_good);
                 very_good.start();
                 break;
             case R.id.button5:
-                // do your code
                 MediaPlayer thanks = MediaPlayer.create(this, R.raw.thanks);
                 thanks.start();
                 break;
 
             case R.id.button6:
-                // do your code
                 MediaPlayer your_welcome = MediaPlayer.create(this, R.raw.your_welcome);
                 your_welcome.start();
                 break;
             case R.id.button7:
-                // do your code
                 MediaPlayer congratulations = MediaPlayer.create(this, R.raw.congratulations);
                 congratulations.start();
                 break;
 
             case R.id.button8:
-                // do your code
                 MediaPlayer see_you_tomorrow = MediaPlayer.create(this, R.raw.see_you_tomorrow);
                 see_you_tomorrow.start();
                 break;
